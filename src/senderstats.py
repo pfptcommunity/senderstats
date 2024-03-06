798cbc87456d3a1eced70f2979cbb54abd0529ee
Ludvik Jerabek <ljerabek@proofpoint.com>
2024-03-06 13:03:37 -0500

    Add message ID parsing
    --no-display for display name stripping
    --exclude-senders for excluding specific senders
    --no-empty-from carry envelope sender to from field when from is empty
    Added sender inferencing by message ID to help determine the originating system.

--- src/senderstats.py
+++ src/senderstats.py
@@ -4,24 +4,48 @@ import datetime
 import os
 import sys
 import re
+from collections import defaultdict
 from glob import glob
-from typing import List
-
+import tldextract
 import xlsxwriter
-
-# EXCLUDE_DOMAINS = r"^({})".format('|'.join(list(PFPT_DOMAINS)))
 
 PROOFPOINT_DOMAIN_EXCLUSIONS = ['ppops.net', 'pphosted.com', 'knowledgefront.com']
 
+# Precompiled Regex Matcher (valid email address) parse group 2
+email_address_re = re.compile(
+    r'(<?\s*([a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@([a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*)\s*)>?\s*(?:;|$))')
 
-def is_valid_domain_syntax(domain_name):
-    pattern = r"(?!-)[A-Za-z0-9-]{1,63}(?<!-)(\.[A-Za-z]{2,63}){1,2}$"
-    if not re.match(pattern, domain_name):
+# Precompiled Regex Matcher (valid domain)
+valid_domain_re = re.compile(r"(?!-)[A-Za-z0-9-]{1,63}(?<!-)(\.[A-Za-z]{2,63}){1,2}$")
+
+
+def is_valid_domain_syntax(domain_name: str):
+    if not valid_domain_re.match(domain_name):
         raise argparse.ArgumentTypeError(f"Invalid domain name syntax: {domain_name}")
     return domain_name
 
 
-def escape_regex_specials(literal_str):
+def is_valid_email_syntax(email: str):
+    if not email_address_re.match(email):
+        raise argparse.ArgumentTypeError(f"Invalid email address syntax: {email}")
+    return email
+
+
+def strip_display_names(email: str):
+    match = email_address_re.search(email)
+    if match:
+        return match.group(2)
+    return email
+
+
+def get_email_domain(email: str):
+    match = email_address_re.search(email)
+    if match:
+        return match.group(3)
+    return email
+
+
+def escape_regex_specials(literal_str: str):
     escape_chars = [".", "*", "+"]
     escaped_text = ""
     for char in literal_str:
@@ -32,15 +56,15 @@ def escape_regex_specials(literal_str):
     return escaped_text
 
 
-def build_or_regex_string(strings: List):
+def build_or_regex_string(strings: list):
     return r"({})".format('|'.join(strings))
 
 
-def average(list: List) -> float:
+def average(list: list) -> float:
     return sum(list) / len(list)
 
 
-def pint(number):
+def pint(number: str):
     if int(number) < 0:
         raise argparse.ArgumentTypeError("'{}' is invalid, integer value must be >= 0".format(number))
     return number
@@ -62,31 +86,45 @@ def main():
                         nargs='+', type=str, required=True,
                         help='Smart search files to read.')
 
-    parser.add_argument('--from-field', metavar='FromField', dest="from_field",
+    parser.add_argument('--hfrom', metavar='FromField', dest="from_field",
                         type=str, required=False, default='Header_From',
                         help='CSV field of the header From: address. (default=Header_From)')
 
-    parser.add_argument('--sender-field', metavar='SenderField', dest="sender_field",
+    parser.add_argument('--mfrom', metavar='SenderField', dest="sender_field",
                         type=str, required=False, default='Sender',
-                        help='CSV field of the From: address. (default=Message_Size)')
+                        help='CSV field of the envelope sender address. (default=Sender)')
 
-    parser.add_argument('--msg-size', metavar='SizeField', dest="size_field",
+    parser.add_argument('--return-path', metavar='ReturnField', dest="return_field",
+                        type=str, required=False, default='Header_Return-Path',
+                        help='CSV field of the Return-Path: address. (default=Header_Return-Path)')
+
+    parser.add_argument('--message-id', metavar='MIDField', dest="mid_field",
+                        type=str, required=False, default='Message_ID',
+                        help='CSV field of the message ID. (default=Message_ID)')
+
+    parser.add_argument('--size', metavar='SizeField', dest="size_field",
                         type=str, required=False, default='Message_Size',
                         help='CSV field of message size. (default=Message_Size)')
 
-    parser.add_argument('--date-field', metavar='DateField', dest="date_field",
+    parser.add_argument('--date', metavar='DateField', dest="date_field",
                         type=str, required=False, default='Date',
-                        help='CSV field of message date. (default=Date)')
+                        help='CSV field of message date/time. (default=Date)')
 
     parser.add_argument('--date-format', metavar='DateFormat', dest="date_format",
                         type=str, required=False, default="%Y-%m-%dT%H:%M:%S.%f%z",
                         help="Date format used to parse the timestamps. (default=%%Y-%%m-%%dT%%H:%%M:%%S.%%f%%z)")
 
-    # parser.add_argument('--filtered-domains', default=[], metavar='<domain>', dest="filtered_domains",
-    #                     nargs='+', type=str, help='Restrict domains to a set of domains.')
+    parser.add_argument('--no-display', action='store_true', dest="no_display",
+                        help='Remove display names, address only')
+
+    parser.add_argument('--no-empty-from', action='store_true', dest="no_empty_from",
+                        help='If the header From: is empty the envelope sender address is used')
 
     parser.add_argument('--excluded-domains', default=[], metavar='<domain>', dest="excluded_domains",
-                        nargs='+', type=is_valid_domain_syntax, help='Restrict domains to a set of domains.')
+                        nargs='+', type=is_valid_domain_syntax, help='Exclude domains from processing.')
+
+    parser.add_argument('--excluded-senders', default=[], metavar='<sender>', dest="excluded_senders",
+                        nargs='+', type=is_valid_email_syntax, help='Exclude senders from processing.')
 
     parser.add_argument('-o', '--output', metavar='<xlsx>', dest="output_file", type=str, required=True,
                         help='Output file')
@@ -97,15 +135,11 @@ def main():
 
     args = parser.parse_args()
 
-    # Handle wildcard expansion
     file_names = []
     for f in args.input_files:
         file_names += glob(f)
 
-    # Remove duplicates
     file_names = set(file_names)
-
-    # Remove all non files resulting from globbing
     file_names = [file for file in file_names if os.path.isfile(file)]
 
     print("Files to be processed:")
@@ -113,26 +147,40 @@ def main():
         print(files)
     print()
 
+    # Remove duplicates + merge
+    args.excluded_domains = list({domain.casefold() for domain in PROOFPOINT_DOMAIN_EXCLUSIONS + args.excluded_domains})
+
+    # Remove duplicates
+    args.excluded_senders = list({sender.casefold() for sender in args.excluded_senders})
+
     print("Domains excluded from processing:")
-    for skip in PROOFPOINT_DOMAIN_EXCLUSIONS + args.excluded_domains:
+    for skip in args.excluded_domains:
         print(skip)
     print()
 
-    # Remove duplicate domain entries from the exclusion list
-    args.excluded_domains = list(
-        {escape_regex_specials(domain.casefold()) for domain in PROOFPOINT_DOMAIN_EXCLUSIONS + args.excluded_domains})
+    print("Senders excluded from processing:")
+    for skip in args.excluded_senders:
+        print(skip)
+    print()
 
+    args.excluded_domains = list({escape_regex_specials(domain.casefold()) for domain in args.excluded_domains})
+    # Pattern used to exclude domains and subdomains
     exclude_pattern = r'(\.|@)' + build_or_regex_string(args.excluded_domains)
-
     exclude_pattern = re.compile(exclude_pattern, flags=re.IGNORECASE)
+
+    # Set used to exclude domains and subdomains
+    skipped_domain_set = set(args.excluded_senders)
 
     total = 0
     skipped = 0
     dates = {}
     sender_data = {}
     from_data = {}
+    return_data = {}
     sender_from_data = {}
-
+    mid_data = {}
+    skipped_senders = defaultdict(int)
+    skipped_domains = defaultdict(int)
     for f in file_names:
         print("Processing: ", f)
         with open(f, 'r', encoding='utf-8-sig') as input_file:
@@ -141,9 +189,45 @@ def main():
                 total += 1
                 env_sender = line[args.sender_field].casefold().strip()
                 header_from = line[args.from_field].casefold().strip()
+                return_path = line[args.return_field].casefold().strip()
+
+                # Message ID is unique but often the sending host behind the @ symbol is unique to the application
+                message_id = line[args.mid_field].casefold().strip()
+                message_id_domain = get_email_domain(message_id)
+                message_id_domain_extract = tldextract.extract(message_id)
+                message_id_host = message_id_domain_extract.subdomain
+                message_id_domain = message_id_domain_extract.domain
+                message_id_domain_suffix = message_id_domain_extract.suffix
+
+                # If header from is empty, we will use env_sender
+                if args.no_empty_from and not header_from:
+                    header_from = env_sender
+
+                # Add domain suffix to TLD
+                if message_id_domain_suffix:
+                    message_id_domain += '.' + message_id_domain_suffix
+
+                if not message_id_host and not message_id_domain_suffix:
+                    message_id_host = message_id_domain
+                    message_id_domain = ''
+
+                if args.no_display:
+                    header_from = strip_display_names(header_from)
+                    return_path = strip_display_names(return_path)
+
                 # Skip empty and domain patterns
                 if exclude_pattern.search(env_sender) or not env_sender:
                     skipped += 1
+                    domain = get_email_domain(env_sender)
+                    if not env_sender:
+                        domain = '(Empty)'
+                    skipped_domains[domain] += 1
+                    continue
+
+                # Skip empty and domain patterns
+                if env_sender in skipped_domain_set:
+                    skipped += 1
+                    skipped_senders[env_sender] += 1
                     continue
 
                 # Determine distinct dates of data, and count number of messages on that day
@@ -161,19 +245,16 @@ def main():
                 else:
                     message_size = 0
 
-                if env_sender not in sender_data:
-                    sender_data[env_sender] = []
-                sender_data[env_sender].append(message_size)
+                sender_data.setdefault(env_sender, []).append(message_size)
+                from_data.setdefault(header_from, []).append(message_size)
+                return_data.setdefault(return_path, []).append(message_size)
 
-                if header_from not in from_data:
-                    from_data[header_from] = []
-                from_data[header_from].append(message_size)
+                # Fat index for binding commonality
+                mid_host_domain_index = (header_from, message_id_host, message_id_domain)
+                mid_data.setdefault(mid_host_domain_index, []).append(message_size)
 
-                # This is useful to identify if senders are aligned properly
                 sender_header_index = (env_sender, header_from)
-                if sender_header_index not in sender_from_data:
-                    sender_from_data[sender_header_index] = []
-                sender_from_data[sender_header_index].append(message_size)
+                sender_from_data.setdefault(sender_header_index, []).append(message_size)
 
     days = len(dates)
 
@@ -216,6 +297,22 @@ def main():
     from_sheet.write(0, 2, "Size", cell_format)
     from_sheet.write(0, 3, "Messages Per Day", cell_format)
     from_sheet.write(0, 4, "Total Bytes", cell_format)
+
+    return_sheet = workbook.add_worksheet("Return Path")
+    return_sheet.write(0, 0, "Return Path", cell_format)
+    return_sheet.write(0, 1, "Messages", cell_format)
+    return_sheet.write(0, 2, "Size", cell_format)
+    return_sheet.write(0, 3, "Messages Per Day", cell_format)
+    return_sheet.write(0, 4, "Total Bytes", cell_format)
+
+    mid_sheet = workbook.add_worksheet("Message ID")
+    mid_sheet.write(0, 0, "Sender", cell_format)
+    mid_sheet.write(0, 1, "Message ID Host", cell_format)
+    mid_sheet.write(0, 2, "Message ID Domain", cell_format)
+    mid_sheet.write(0, 3, "Messages", cell_format)
+    mid_sheet.write(0, 4, "Size", cell_format)
+    mid_sheet.write(0, 5, "Messages Per Day", cell_format)
+    mid_sheet.write(0, 6, "Total Bytes", cell_format)
 
     sender_from_sheet = workbook.add_worksheet("Sender + From (Alignment)")
     sender_from_sheet.write(0, 0, "Sender", cell_format)
@@ -296,6 +393,28 @@ def main():
         from_sheet.write_formula(line, 4, "=B{line}*C{line}".format(line=line + 1))
         line += 1
 
+    # Write data to return from worksheet
+    line = 1
+    for k, v in return_data.items():
+        return_sheet.write(line, 0, k)
+        return_sheet.write(line, 1, len(v))
+        return_sheet.write(line, 2, average(v))
+        return_sheet.write_formula(line, 3, "=B{line}/{days}".format(line=line + 1, days=days))
+        return_sheet.write_formula(line, 4, "=B{line}*C{line}".format(line=line + 1))
+        line += 1
+
+    # Write data to return from worksheet
+    line = 1
+    for k, v in mid_data.items():
+        mid_sheet.write(line, 0, k[0])
+        mid_sheet.write(line, 1, k[1])
+        mid_sheet.write(line, 2, k[2])
+        mid_sheet.write(line, 3, len(v))
+        mid_sheet.write(line, 4, average(v))
+        mid_sheet.write_formula(line, 5, "=D{line}/{days}".format(line=line + 1, days=days))
+        mid_sheet.write_formula(line, 6, "=D{line}*E{line}".format(line=line + 1))
+        line += 1
+
     line = 1
     # Write data to envelope worksheet
     for k, v in sender_from_data.items():
@@ -309,15 +428,26 @@ def main():
 
     workbook.close()
 
+    print()
     print("File Processing Summary")
     print("Total Records: ", total)
     print("Skipped Records: ", skipped)
-    print("")
+    print()
+
+    print("Skipped Domain Details")
+    for k, v in skipped_domains.items():
+        print("Skipped {}:".format(k), v)
+    print()
+
+    print("Skipped Sender Details")
+    for k, v in skipped_senders.items():
+        print("Skipped {}:".format(k), v)
+    print()
 
     print("Records by Day")
-
     for d in sorted(dates.keys()):
-        print("{}:".format(d),dates[d])
+        print("{}:".format(d), dates[d])
+    print()
 
     print("Please see report: {}".format(args.output_file))
 
