547307e7b586156b47d93e45d7adc7e0a2227dc4
Ludvik Jerabek <ljerabek@proofpoint.com>
2024-03-29 16:46:52 -0400

    Revert "Update usage information"
    This reverts commit 8cbf35e51b25f6ea7f539f7fc6dfe963d8638e62.

--- src/senderstats/cli.py
+++ src/senderstats/cli.py
@@ -67,8 +67,8 @@ def parse_arguments():
     reporting_group.add_argument('--gen-msgid', action='store_true', dest="gen_msgid",
                                  help='Generate report showing parsed Message ID. Helps determine the sending system')
 
-    reporting_group.add_argument('-t', '--threshold', dest="threshold", type=int, required=False,
-                                 help=f'Integer representing number of messages per day to be considered application traffic. (default={DEFAULT_THRESHOLD})',
+    reporting_group.add_argument('-t', '--threshold', dest="threshold", metavar='N', type=int, required=False,
+                                 help=f'Adjust summary report threshold for messages per day to be considered application traffic. (default={DEFAULT_THRESHOLD})',
                                  default=DEFAULT_THRESHOLD)
 
     parser_group.add_argument('--no-display-name', action='store_true', dest="no_display",
