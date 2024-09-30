2565fb75b8ea5a390d20aa5e6f1e7c56b36b42df
Ludvik Jerabek <ljerabek@proofpoint.com>
2024-09-30 15:24:31 -0400

    Fix include all objects

--- src/senderstats/data/processors/__init__.py
+++ src/senderstats/data/processors/__init__.py
@@ -1,4 +1,5 @@
 from .AlignmentProcessor import AlignmentProcessor
+from .DateProcessor import DateProcessor
 from .HFromProcessor import HFromProcessor
 from .MFromProcessor import MFromProcessor
 from .MIDProcessor import MIDProcessor
@@ -6,6 +7,7 @@ from .RPathProcessor import RPathProcessor
 
 __all__ = [
     'AlignmentProcessor',
+    'DateProcessor',
     'HFromProcessor',
     'MFromProcessor',
     'MIDProcessor',
