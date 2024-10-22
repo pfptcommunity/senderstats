ffb2a45dc84cd86af0bbf0f43ad7b6a5d90bf220
Ludvik Jerabek <ljerabek@proofpoint.com>
2024-10-22 10:57:48 -0400

    Decouple managers

---
+++ src/senderstats/processing/FilterManager.py
@@ -0,0 +1,9 @@
+from senderstats.processing.ExclusionManager import ExclusionManager
+from senderstats.core.filters import *
+
+class FilterManager:
+    def __init__(self, exclusion_manager: ExclusionManager):
+        self.exclude_empty_sender_filter = ExcludeEmptySenderFilter()
+        self.exclude_domain_filter = ExcludeDomainFilter(exclusion_manager.excluded_domains)
+        self.exclude_senders_filter = ExcludeSenderFilter(exclusion_manager.excluded_senders)
+        self.restrict_senders_filter = RestrictDomainFilter(exclusion_manager.restricted_domains)
