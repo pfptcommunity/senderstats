fd96132464aacb6ddbccc2c0eeeb5bc0d67603c9
Ludvik Jerabek <ljerabek@proofpoint.com>
2024-10-22 13:29:51 -0400

    Add option to disable default Proofpoint domain filters

--- src/senderstats/processing/ExclusionManager.py
+++ src/senderstats/processing/ExclusionManager.py
@@ -4,7 +4,9 @@ from senderstats.common.defaults import DEFAULT_DOMAIN_EXCLUSIONS
 class ExclusionManager:
     def __init__(self, args):
         self.excluded_senders = self.__prepare_exclusions(args.excluded_senders)
-        self.excluded_domains = self.__prepare_exclusions(DEFAULT_DOMAIN_EXCLUSIONS + args.excluded_domains)
+        if not args.no_default_exclude_domains:
+            args.excluded_domains = DEFAULT_DOMAIN_EXCLUSIONS + args.excluded_domains
+        self.excluded_domains = self.__prepare_exclusions(args.excluded_domains)
         self.restricted_domains = self.__prepare_exclusions(args.restricted_domains)
 
     def __prepare_exclusions(self, exclusions):
