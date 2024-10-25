cdd9c52499b22b282208d0d2d05b9d52cd9d1eff
Ludvik Jerabek <ljerabek@proofpoint.com>
2024-10-25 12:54:24 -0400

    Fix bug after argument rename

--- src/senderstats/processing/ExclusionManager.py
+++ src/senderstats/processing/ExclusionManager.py
@@ -5,7 +5,7 @@ class ExclusionManager:
     def __init__(self, args):
         self.excluded_senders = self.__prepare_exclusions(args.exclude_senders)
         if not args.no_default_exclude_domains:
-            args.excluded_domains = DEFAULT_DOMAIN_EXCLUSIONS + args.exclude_domains
+            args.exclude_domains = DEFAULT_DOMAIN_EXCLUSIONS + args.exclude_domains
         self.excluded_ips = self.__prepare_exclusions(args.exclude_ips)
         self.excluded_domains = self.__prepare_exclusions(args.exclude_domains)
         self.restricted_domains = self.__prepare_exclusions(args.restrict_domains)
