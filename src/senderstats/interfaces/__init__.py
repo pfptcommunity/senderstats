2efaf9df1e4fd9372bde83c8304aaceb6fe6c349
Ludvik Jerabek <ljerabek@proofpoint.com>
2024-10-28 11:01:10 -0400

    Update __init__.py

--- src/senderstats/interfaces/__init__.py
+++ src/senderstats/interfaces/__init__.py
@@ -3,6 +3,7 @@ from .handler import Handler
 from .processor import Processor
 from .transform import Transform
 from .validator import Validator
+from .reportable import Reportable
 
 __all__ = [
     'Filter',
