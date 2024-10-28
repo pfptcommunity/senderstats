c8fa0c1f84d9b35faa4684b4b73d443d5fbb4b8f
Ludvik Jerabek <ljerabek@proofpoint.com>
2024-10-28 11:41:38 -0400

    Fix case

--- src/senderstats/interfaces/__init__.py
+++ src/senderstats/interfaces/__init__.py
@@ -3,13 +3,11 @@ from .handler import Handler
 from .processor import Processor
 from .transform import Transform
 from .validator import Validator
-from .reportable import Reportable
 
 __all__ = [
     'Filter',
     'Handler',
     'Processor',
     'Transform',
-    'Validator',
-    'Reportable'
+    'Validator'
 ]
