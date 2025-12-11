4dd8c82f785ffb5d950929dd250deac223d71f3a
Ludvik Jerabek <ljerabek@pfpt.io>
2025-12-10 21:35:14 -0500

    Fix python -m

--- src/senderstats/__main__.py
+++ src/senderstats/__main__.py
@@ -2,9 +2,12 @@ import sys
 from senderstats.cli import main as cli_main
 from senderstats.gui import main as gui_main
 
-if __name__ == "__main__":
+def main():
     if "--gui" in sys.argv:
         sys.argv.remove("--gui")
         gui_main()
     else:
         cli_main()
+
+if __name__ == "__main__":
+    main()
